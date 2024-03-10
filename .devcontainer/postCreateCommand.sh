7bc4f821840148e3009605097d353665b41773b3
Takanobu Amano <46679145+amanotk@users.noreply.github.com>
2024-03-10 15:29:01 +0000

    Added gnuplot sample notebook

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,8 +1,9 @@
-# python modules
-python3 -m pip install -r .devcontainer/requirements.txt
-
 # Ubuntu packages
 apt-get update
 apt-get install -y \
     gfortran \
-    gnuplot
+    gnuplot-nox
+
+# python modules
+python3 -m pip install -r .devcontainer/requirements.txt
+python3 -m gnuplot_kernel install

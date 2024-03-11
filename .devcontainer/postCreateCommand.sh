65e8c0c2cc1efbfe635cef8950133584c66649bf
Takanobu Amano <46679145+amanotk@users.noreply.github.com>
2024-03-11 13:21:02 +0000

    Added fortran kadai answer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,6 +5,7 @@ cd $WORKSPACE
 if [ ! -d "fortran" ]; then
     mkdir fortran && cd fortran
     git clone https://github.com/amanotk/fortran-resume-sample.git sample
+    git clone https://github.com/amanotk/fortran-resume-answer.git answer
 fi
 
 cd $WORKSPACE
